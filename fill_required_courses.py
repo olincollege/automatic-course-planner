ddb7e57fbeb82bafb655e4e889ca4373412e72cc
delee5695 <delee5695@naver.com>
2024-04-26 13:56:47 -0400

    update fill_required_course - check if the required course is took everytimestep - works for MechE - fix meche prediction

--- fill_required_courses.py
+++ fill_required_courses.py
@@ -1,20 +1,24 @@
 import pandas as pd
+import warnings
+
+# Settings the warnings to be ignored
+warnings.filterwarnings("ignore")
+
 
 sem_courses = {
     "Freshmen Fall": ["QEA1", "Modsim", "DesNat", "AHS"],
-    "Freshmen Spring": ["QEA2", "ISIM", "P&M", "SoftDes"],
+    "Freshmen Spring": [
+        "QEA2",
+        "ISIM",
+        "P&M",
+    ],
     "Sophomore Fall": [
         "PIE",
-        "Discrete",
     ],
     "Sophomore Spring": [
         "CD",
-        "SoftSys",
     ],
-    "Junior Fall": [
-        "Bio",
-        "MatSci",
-    ],
+    "Junior Fall": [],
     "Junior Spring": [],
     "Senior Fall": [
         "Capstone",
@@ -31,13 +35,8 @@ courses_took = [
     "QEA2",
     "ISIM",
     "P&M",
-    "SoftDes",
     "PIE",
-    "Discrete",
     "CD",
-    "SoftSys",
-    "Bio",
-    "MatSci",
 ]
 major_requirements = {
     "E:C": {
@@ -45,48 +44,62 @@ major_requirements = {
         "ENGR3525": [False, 1],
         "ENGR3599A/ENGR3520": [False, 1],
     },
-    "MechE": {},
+    "MechE": {
+        "ENGX2010": [False, 0],
+        "ENGR2320": [False, 0],
+        "ENGX2134": [False, 0],
+        "ENGR2340": [False, 1],
+        "ENGR2360": [False, 1],
+        "ENGR3330": [False, 1],
+        "ENGR3110/ENGR3180/ENGR3232/ENGR3299C/ENGR3260/ENGR3345/ENGR3350/ENGR3370/ENGR3392/ENGR3820": [
+            False,
+            1,
+        ],
+        "MTH3120/MTH3150/MTH3170": [False, 1],
+    },
 }
 electives = {
     "E:C": ["ENGR3220", "ENGR3410", "ENGR3540", "ENGR3590", "ENGR3599"],
-    "MechE": [],
 }
 df = pd.read_csv("predicted_schedule.csv", index_col=0).T
 max_num_course = 4
-major = "E:C"
-last_level = 1
+major = "MechE"
+last_level = 0
 for courses, [took, level] in major_requirements[major].items():
-    if not took:
-        courses_separated = courses.split("/")
-        for course in courses_separated:
-            # print(f'{course}: {df[course]}')
-            offering_semester_prediction = df[course].to_frame().T
-            offering_semester_prediction.set_index("Course Title", inplace=True)
-            offered_semesters = [
-                semester
-                for semester, value in offering_semester_prediction.items()
-                if float(value) >= 0.5
-            ]
-            print(f"{course}:{offered_semesters}")
-            # look through the sem_courses
-            for idx, [semester, took_courses] in enumerate(sem_courses.items()):
-                # skip freshman years
-                if idx < 3:
-                    continue
-                # if a semester in sem_courses have less than max num courses
-                # and is in the offered_semesters list and last_level is not higher than current level, fill it out from the start
-                if (
-                    semester.lower() in offered_semesters
-                    and len(took_courses) < max_num_course
-                    and last_level <= level
-                    and course not in courses_took
-                ):
-                    sem_courses[semester].append(df[course]["Course Title"])
-                    # change took to True
-                    major_requirements[major][courses][0] = True
-                    # add the course number to courses_took
-                    courses_took.append(course)
-                    # update level
-                    last_level = level
-                    continue
+    courses_separated = courses.split("/")
+    for course in courses_separated:
+        # print(f'{course}: {df[course]}')
+        offering_semester_prediction = df[course].to_frame().T
+        offering_semester_prediction.set_index("Course Title", inplace=True)
+        offered_semesters = [
+            semester
+            for semester, value in offering_semester_prediction.items()
+            if float(value) >= 0.5
+        ]
+        course_title = df[course]["Course Title"]
+        print(course_title, offering_semester_prediction)
+
+        print(f"{course_title}:{offered_semesters}")
+        # look through the sem_courses
+        for idx, [semester, took_courses] in enumerate(sem_courses.items()):
+            # skip freshman fall
+            if idx < 2:
+                continue
+            # if a semester in sem_courses have less than max num courses
+            # and is in the offered_semesters list and last_level is not higher than current level, fill it out from the start
+            if (
+                semester.lower() in offered_semesters
+                and len(took_courses) < max_num_course
+                and last_level <= level
+                and course not in courses_took
+                and major_requirements[major][courses][0] is False
+            ):
+                sem_courses[semester].append(df[course]["Course Title"])
+                # change took to True
+                major_requirements[major][courses][0] = True
+                # add the course number to courses_took
+                courses_took.append(course)
+                # update level
+                last_level = level
+                continue
 print(sem_courses)
